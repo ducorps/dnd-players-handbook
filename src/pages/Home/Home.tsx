8adb1dd470589314bab94c05c139a7b9dfa4c37e
Sofia <sofia.martinslv@gmail.com>
2023-11-26 22:38:57 -0300

    wip-pagination

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,31 +7,51 @@ import { useEffect, useState } from "react";
 
 export function Home() {
   const [characterList, setCharacterList] = useState([]);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(3);
+  const [maxPages, setMaxPages] = useState(3);
   const navigate = useNavigate();
 
   async function handleCreateNewCharacter() {
-    setLoading(true)
-    await api.post("/characters/new-character")
-      .then((response) => navigate(`/character/${response.data.id}/create`))
-    setLoading(false)
+    setLoading(true);
+    await api
+      .post("/characters/new-character")
+      .then((response) => navigate(`/character/${response.data.id}/create`));
+    setLoading(false);
   }
 
   async function getCharactersList() {
-    await api.get("characters/user")
-      .then((res: any) => {
-        console.log(res.data);
-        setCharacterList(res.data)
-      })
+    await api.get("characters/user").then((res: any) => {
+      console.log(res.data);
+      setCharacterList(res.data);
+    });
+  }
+
+  async function handleNextPage() {
+    if(page < maxPages) {
+      const newPage = page + 1;
+      setPage(newPage);
+    }
+  }
+
+  async function handlePreviousPage() {
+    if (page != 1) {
+      const newPage = page - 1;
+      setPage(newPage);
+    }
   }
 
   useEffect(() => {
-    getCharactersList()
-  },[])
+    getCharactersList();
+  }, []);
 
   return (
     <div className={styles.container}>
-      <button disabled={loading} className={styles.newCharacter} onClick={handleCreateNewCharacter}>
+      <button
+        disabled={loading}
+        className={styles.newCharacter}
+        onClick={handleCreateNewCharacter}
+      >
         <h2>Criar novo</h2>
         <h2 style={{ fontWeight: "bold" }}>+</h2>
       </button>
@@ -55,19 +75,20 @@ export function Home() {
             </div>
           </Link>
         </div>
-        {characterList.map((item: any)=>(
+        {characterList.map((item: any) => (
           <div key={item.id} className={styles.card}>
             <div className={styles.img}></div>
             <div>
               <Tooltip tooltip={item.name}>
-                <p className={styles.charName}>
-                  {item.name}
-                </p>
+                <p className={styles.charName}>{item.name}</p>
               </Tooltip>
               <span style={{ fontWeight: "bold" }}>{item.race?.raceType}</span>
               <span>{item.classType?.classType}</span>
             </div>
-            <Link className={styles.accountButton} to={`/character/${item.id}/sheet`}>
+            <Link
+              className={styles.accountButton}
+              to={`/character/${item.id}/sheet`}
+            >
               <div className={styles.playButton}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
                   <path d="M240,128a15.74,15.74,0,0,1-7.6,13.51L88.32,229.65a16,16,0,0,1-16.2.3A15.86,15.86,0,0,1,64,216.13V39.87a15.86,15.86,0,0,1,8.12-13.82,16,16,0,0,1,16.2.3L232.4,114.49A15.74,15.74,0,0,1,240,128Z"></path>
@@ -76,6 +97,11 @@ export function Home() {
             </Link>
           </div>
         ))}
+      </div>
+      <div className={styles.pagination}>
+        <img onClick={handlePreviousPage} src="/src/assets/chavron-left.svg" />
+        <p>{page} de {maxPages}</p>
+        <img onClick={handleNextPage} src="/src/assets/chavron-right.svg" />
       </div>
     </div>
   );
